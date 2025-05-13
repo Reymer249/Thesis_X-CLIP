f8acf910555b466816b458e55a2c3ce3a7db7c04
Max <m.i.lytovka@umail.leidenuniv.nl>
2025-05-13 16:10:18 +0200

    Changed scripts for vibranium

--- scripts/generate_hard_negatives-positives/generate_hard_positives_llm.sh
+++ scripts/generate_hard_negatives-positives/generate_hard_positives_llm.sh
@@ -1,5 +1,5 @@
 python /home/s3705609/thesis_code/helpers_scripts/hard_negatives-positives_generation/generate_hard_positives-negatives_llm.py \
-  --captions_file /vol/home/s3705609/Desktop/data_vatex/splits_txt/captions_avail_formatted.json \
-  --video_ids_file /vol/home/s3705609/Desktop/data_vatex/splits_txt/test.txt \
+  --captions_file /data/s3705609/VATEX/splits_txt/captions_avail_formatted.json \
+  --video_ids_file /data/s3705609/VATEX/splits_txt/test.txt \
   --num_sentences 20 \
   --model_name Qwen/Qwen2.5-1.5B-Instruct
