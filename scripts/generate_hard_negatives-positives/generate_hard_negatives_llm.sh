4cf5155bab1cdbf8062d4ff5138d71d2c5480bda
Max <m.i.lytovka@umail.leidenuniv.nl>
2025-05-13 15:12:37 +0200

    Code for llm generation is almost-almost ready

--- scripts/generate_hard_negatives-positives/generate_hard_negatives_llm.sh
+++ scripts/generate_hard_negatives-positives/generate_hard_negatives_llm.sh
@@ -2,4 +2,5 @@ python /vol/home/s3705609/Desktop/thesis_code/helpers_scripts/hard_negatives-pos
   --captions_file /vol/home/s3705609/Desktop/data_vatex/splits_txt/captions_avail_formatted.json \
   --video_ids_file /vol/home/s3705609/Desktop/data_vatex/splits_txt/test.txt \
   --num_sentences 20 \
-  --model_name Qwen/Qwen2.5-1.5B-Instruct
+  --model_name Qwen/Qwen2.5-1.5B-Instruct \
+  --do_neg_gen
