543502239a56009d1e8588ea66c407bd63c44ae0
Max <m.i.lytovka@umail.leidenuniv.nl>
2025-05-16 09:47:16 +0200

    fill fill hard negatives

--- scripts/generate_hard_negatives-positives/fill_gaps_generate_hard_negatives_llm.sh
+++ scripts/generate_hard_negatives-positives/fill_gaps_generate_hard_negatives_llm.sh
@@ -1,7 +1,7 @@
 python /home/s3705609/thesis_code/helpers_scripts/hard_negatives-positives_generation/fill_gaps_generate_hard_positives-negatives_llm.py \
   --captions_file /data/s3705609/VATEX/splits_txt/captions_train_avail_020.json \
-  --generated_file /data/s3705609/VATEX/splits_txt/hard_negatives_llm.json \
+  --generated_file /home/s3705609/thesis_code/hard_negatives_llm_filled.json \
   --num_sentences 20 \
   --model_name Qwen/Qwen2.5-1.5B-Instruct \
-  --batch_size 128 \
+  --batch_size 32 \
   --gen_hard_neg
